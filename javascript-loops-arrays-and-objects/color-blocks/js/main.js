6255bc171e4a419df0f0033cd082029bee1adc67
James Reynolds <jameshreynolds@gmail.com>
2017-10-19 10:17:06 -0700

    Modified color-blocks project to utilize a random color function.

--- javascript-loops-arrays-and-objects/color-blocks/js/main.js
+++ javascript-loops-arrays-and-objects/color-blocks/js/main.js
@@ -1,17 +1,17 @@
 var html = '';
-var red;
-var green;
-var blue;
-var rgbColor;
 
-for (i = 0; i < 10; i++) {
-	red = Math.floor(Math.random() * 256 );
-	green = Math.floor(Math.random() * 256 );
-	blue = Math.floor(Math.random() * 256 );
-	rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-	html += '<div style="background-color:' + rgbColor + '"></div>';
+
+function randomColor() {
+	var red = Math.floor(Math.random() * 256 );
+	var green = Math.floor(Math.random() * 256 );
+	var blue = Math.floor(Math.random() * 256 );
+	var rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+	return rgbColor;
 }
 
+for (i = 0; i < 10; i++) {
+	html += '<div style="background-color:' + randomColor() + '"></div>';
+}
 
 
 document.write(html);
