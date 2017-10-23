0742675161db34bd0e66c98633dd007427b9fcc5
James Reynolds <jameshreynolds@gmail.com>
2017-10-22 21:32:35 -0700

    Added for in loop example. Very important for search objects for property and or values.

---
+++ javascript-loops-arrays-and-objects/for-in-loop/js/main.js
@@ -0,0 +1,20 @@
+// for in loop
+var person = {
+  name : 'Sarah',
+  country : 'US',
+  age : 35,
+  treehouseStudent : true,
+  skills : ['JavaScript', 'HTML', 'CSS']
+};
+
+// 'prop' acts like a variable it could be named key or anything else
+for (prop in person) {
+	// console feature is letting me print multiple string, so print property name, then a ":" string and then property value
+	console.log(prop, ":", person[prop])
+}
+
+
+for (prop in person) {
+	// The ":" string was not necessary it just looks cool, also note that [] notation is necessary. dot notation will not work to get the value in this case
+	console.log(prop, person[prop])
+}
