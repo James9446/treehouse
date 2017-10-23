5fb81277acc47169280badcf089ececf9727f14f
James Reynolds <jameshreynolds@gmail.com>
2017-10-23 16:27:19 -0700

    Added next step in build object challenge. Making the array of objects searchable using the prompt method and printing the results to the console.

---
+++ javascript-loops-arrays-and-objects/build-object-challenge-searchable/js/main.js
@@ -0,0 +1,34 @@
+// Treehouse challenge
+
+var students = [
+	{
+		name: "James",
+		track: "Front-end development",
+		achievements: 10,
+		points: 100
+	},
+	{
+		name: "Mike",
+		track: "Front-end development",
+		achievements: 5,
+		points: 60
+	},
+	{
+		name: "Mary",
+		track: "iOS",
+		achievements: 20,
+		points: 250
+	},
+	{
+		name: "Sam",
+		track: "Web Design",
+		achievements: 15,
+		points: 175
+	},
+	{
+		name: "Jessie",
+		track: "Back-end development",
+		achievements: 10,
+		points: 100
+	}
+]
